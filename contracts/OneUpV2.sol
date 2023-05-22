02cdaaea56d2ab1621429ce3844926f83f9f674c
Alexandre Collette <alexcollette@hotmail.com>
2023-05-22 12:26:46 +0200

    claimRewardsFromDelegate() updated

--- contracts/OneUpV2.sol
+++ contracts/OneUpV2.sol
@@ -21,57 +21,11 @@ interface IMultiFarmingPod {
     function claim() external;
 }
 
-interface IBalancerPoolCreationHelper {
-    function initJoinStableSwap(
-        bytes32 poolId,
-        address poolAddress,
-        address[] memory tokenAddresses,
-        uint256[] memory weiAmountsPerToken
-    ) external;
-}
-
-interface IBalancerPool {
-    function getPoolId() external view returns(bytes32);
-    function getActualSupply() external view returns (uint256);
-}
-
-interface IBalancerVault {
-    function getPoolTokens(bytes32) external view returns (address[] memory, uint256[] memory, uint256);
-    function joinPool(
-        bytes32 poolId,
-        address sender,
-        address recipient,
-        JoinPoolRequest memory request
-    ) external payable;
-    function exitPool(
-        bytes32 poolId,
-        address sender,
-        address recipient,
-        ExitPoolRequest memory request
-    ) external payable;
-}
-
 interface IMultiRewards {
     function addReward(address, address, uint256) external;
     function notifyRewardAmount(address _rewardsToken, uint256 reward) external;
     function stake(uint256 amount) external;
     function stakeFor(uint256 amount, address account) external;
-}
-
-/// @dev The struct to provide to the Balancer contract call JoinPool().
-struct JoinPoolRequest {
-    address[] assets;
-    uint256[] maxAmountsIn;
-    bytes userData;
-    bool fromInternalBalance;
-}
-
-/// @dev The struct to provide to the Balancer contract call ExitPool().
-struct ExitPoolRequest {
-    address[] assets;
-    uint256[] minAmountsOut;
-    bytes userData;
-    bool toInternalBalance;
 }
 
 
@@ -88,14 +42,10 @@ contract OneUpV2 is ERC20 {
     address immutable public stake1inch = 0x9A0C8Ff858d273f57072D714bca7411D717501D7;  
     address immutable public powerPod = 0xAccfAc2339e16DC80c50d2fa81b5c2B049B4f947;
     address immutable public resolverFarmingPod = 0x7E78A8337194C06314300D030D41Eb31ed299c39;
-    address immutable public balancerPoolCreationHelper = 0xa289a03f46f144fAaDd9Fc51b006d7322ECc9B04;
-    address immutable public balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
     
 
     bool public vaultStarted;               /// @dev Will be set to "true" after first deposit 
-    bool public poolAddressSet;             /// @dev Returns "true" once the Balancer Pool address has been set
-    bool public poolInitialized;            /// @dev Returns "true" once initial liquidity has been provided to the pool
-    bool public poolEnded;                  /// @dev Pool ends when all 1inch tokens are unstaked after duration period
+    bool public vaultEnded;                  /// @dev Vault ends when all 1inch tokens are unstaked after duration period
     address public delegatee;               /// @dev The address of the current delegatee
     address public balancerPool;            /// @dev The 1inch/1UP Curve Pool
     address public stakingContract;
@@ -120,7 +70,7 @@ contract OneUpV2 is ERC20 {
     /// @param amount The amount of assets to deposit.
     /// @return shares The number of shares that have been minted.
     function deposit(uint256 amount, bool stake) public {
-        require(poolEnded == false, "Pool ended");
+        require(vaultEnded == false, "Vault ended");
 
         uint256 duration;
 
@@ -156,54 +106,17 @@ contract OneUpV2 is ERC20 {
 
     }
 
-    /// @notice This function will claim rewards from the delegates and provide liquidity in the Balancer pool.
+    /// @notice This function will claim rewards from the delegates and add the rewards to the staking contract
+    /// TODO: when pool is closed not able to call fct anymore.
+    /// TODO: add a earned fct to view rewards claimable
     function claimRewardsFromDelegate() public {
-        require(poolInitialized == true, "Make an initial deposit to the Balancer pool before claiming");
         IMultiFarmingPod(resolverFarmingPod).claim();
-        
-        bytes32 poolId = IBalancerPool(balancerPool).getPoolId();
-        uint256 toDeposit = IERC20(address(oneInchToken)).balanceOf(address(this));
-
-        IERC20(address(oneInchToken)).safeApprove(stakingContract, toDeposit);
-        IBalancerVault(balancerVault).joinPool(poolId, address(this), address(this), request);
+        uint256 toDeposit = oneInchToken.balanceOf(address(this));
+        oneInchToken.safeApprove(stakingContract, toDeposit);
+        IMultiRewards(stakingContract).notifyRewardAmount(address(oneInchToken), toDeposit);
     }
 
-    /// @notice Sets the Balancer 1inch/1UP pool address for this contract and initializes the pool with first deposit.
-    /// @param _balancerPool The address of the Balancer pool linked to this contract.
-    function setBalancerPool(address _balancerPool) public {
-        require(poolAddressSet == false, "Balancer pool already set");
 
-        poolAddressSet == true;
-        balancerPool = _balancerPool;
-    }
-
-    /// @notice This function will initialize the Balancer pool by providing the first liquidity.
-    /// @param amounts The amount of each token to add for the initialization of the Balancer pool.
-    function initBalancerPool(uint256[] memory amounts) public {
-        require(poolInitialized == false, "Balancer pool already intialized");
-        require(amounts[0] >= 1_000 ether && amounts[1] >= 1_000 ether, "insufficient amounts");
-
-        poolInitialized = true;
-
-        IERC20(address(oneInchToken)).safeTransferFrom(_msgSender(), address(this), amounts[0]);
-        IERC20(address(this)).safeTransferFrom(_msgSender(), address(this), amounts[1]);
-
-        address[] memory tokenAddresses = new address[](2);
-        tokenAddresses[0] = address(oneInchToken);
-        tokenAddresses[1] = address(this);
-
-        bytes32 poolId = IBalancerPool(balancerPool).getPoolId();
-
-        IERC20(address(oneInchToken)).safeApprove(balancerPoolCreationHelper, amounts[0]);
-        IERC20(address(this)).safeApprove(balancerPoolCreationHelper, amounts[1]);
-
-        IBalancerPoolCreationHelper(balancerPoolCreationHelper).initJoinStableSwap(
-            poolId, 
-            balancerPool, 
-            tokenAddresses, 
-            amounts
-        );
-    }
 
     /// @notice This function will unstake 1inch tokens after duration ends and remove liquidity from the Balancer pool.
     /// @param assets The amount of the asset to withdraw. 
